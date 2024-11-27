48ff33c9ede983bcf81ff857775420f29b6677c1
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2024-11-27 22:00:56 +0000

    remove app

--- tanstack-start-version/app.config.ts
+++ tanstack-start-version/app.config.ts
@@ -2,9 +2,6 @@
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-// validate on build
-import { env as _env } from "./app/env";
-
 export default defineConfig({
   server: {
     preset: "vercel",
